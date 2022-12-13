d96c9fada5d482405f00312bdbeba85d55c62d0d
CameronAulton <113976873+CameronAulton@users.noreply.github.com>
2022-12-13 09:44:36 +0000

    Add files via upload

---
+++ NameandAge.py
@@ -0,0 +1,17 @@
+Forename ="Cameron"
+Surname = "Aulton"
+
+Age = 16
+
+Fullname = Forename + " " + Surname
+
+print("Your first name is " + Forename)
+print("Your surname is " + Surname)
+
+print("This year you are " + str(Age) + " years old.")
+
+Age += 1
+
+print("Next year you will be " + str(Age) + "years old.")
+
+print("end of application.....")
